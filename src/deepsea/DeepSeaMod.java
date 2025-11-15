dbfe696c65bd4f95ad3916485002c6d1f8c45d1f
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-11-15 15:57:59 +0300

    deepsea.java

---
+++ src/deepsea/DeepSeaMod.java
@@ -0,0 +1,16 @@
+package deepsea;
+
+import arc.util.*;
+import mindustry.mod.*;
+
+public class DeepSeaMod extends Mod{
+
+    public DeepSeaMod(){
+        Log.info("Loaded Deep Sea' constructor.");
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading Deep Sea.");
+    }
+}
