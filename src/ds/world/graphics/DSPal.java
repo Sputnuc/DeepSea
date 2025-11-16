928ae44fca47f61f254d509d427bce44b96e9b3d
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-11-16 15:45:21 +0300

    Create DSPal.java

---
+++ src/ds/world/graphics/DSPal.java
@@ -0,0 +1,10 @@
+package ds.world.graphics;
+
+import arc.graphics.*;
+
+public class DSPal{
+    public static Color
+    teastPal1 = Color.valueOf("262229"),
+    testPal2 = Color.valueOf("212222")
+    ;
+}
